eccd96e4d2b0ed67267b7aa156059b27ce99c6a2
Thomas Beguin <thom.beguin@gmail.com>
2022-11-06 15:43:09 +0000

    added helper tests

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,21 @@
+require "test_helper"
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "layout links" do
+    #Get the root path (Home page).
+    get root_path
+
+    #Verify that the right page template is rendered.
+    assert_template 'static_pages/home'
+
+    #Check for the correct links to the Home, Help, About, and Contact pages.
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    #Rails inserts the path in place of the question mark, and checks if there
+    #is an HTML tag of the form <a href="">...<a/>
+    get contact_path
+    assert_select "title", full_title("Contact")
+  end
+end
